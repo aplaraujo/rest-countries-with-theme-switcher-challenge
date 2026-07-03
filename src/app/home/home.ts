ed73582002365bb7dee00f407649411a7af5cd7b
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-07-03 15:58:25 -0300

    Busca por país

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,32 +1,58 @@
-import { Component,  inject } from '@angular/core';
+import { Component,  inject, OnInit } from '@angular/core';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { Observable, of } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  switchMap,
+  startWith,
+} from 'rxjs/operators';
 import { Header } from '../header/header';
 import { CountryService } from '../country-service';
 import { ICountry } from '../country';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { RegionFilterPipe } from '../region-filter-pipe';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, RegionFilterPipe, ReactiveFormsModule],
+  imports: [Header, RegionFilterPipe, ReactiveFormsModule, CommonModule, AsyncPipe],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
+export class Home implements OnInit{
   countryList: ICountry[] = [];
   countryService: CountryService = inject(CountryService);
+  filteredCountries!: Observable<any[]>;
   
   constructor() {
     this.countryList = this.countryService.getAllCountries();
   }
 
-  regionArr:string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  regionArr:string[] = ['Africa', 'Americas', 'Antarctic Ocean' ,'Asia', 'Europe', 'Oceania'];
   regionOption = '';
+  countryText = '';
 
-  countryForm = new FormGroup({
-    region: new FormControl(this.regionOption)
-  });
-
+  region = new FormControl(this.regionOption);
+  country = new FormControl(this.countryText);
+  
   handleSelect(event: any) {
     return this.regionOption = (event.target as HTMLInputElement).value;
+  }
+
+  ngOnInit() {
+    this.filteredCountries = this.country.valueChanges.pipe(
+      startWith(''),
+      debounceTime(300),
+      distinctUntilChanged(),
+      map((term) => term ? term.trim().toLowerCase() : ''),
+      switchMap((term) => {
+        if (!term) {
+          return of(this.countryList)
+        }
+
+        return of(this.countryList.filter((country) => country.name.toLowerCase().includes(term)));
+      })
+    )
   }
 }
