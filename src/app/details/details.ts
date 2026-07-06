12239daab30778c9fe636438b3fb367ff31fe6fe
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-07-06 08:16:54 -0300

    Foco nos elementos interativos

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CountryService } from '../country-service';
+import { ICountry } from '../country';
+import { Header } from '../header/header';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [Header],
   templateUrl: './details.html',
   styleUrl: './details.scss',
 })
 export class Details {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  countryService = inject(CountryService);
+  country: ICountry | undefined;
 
+  constructor() {
+    const countryAlphaCode = this.route.snapshot.params['alpha3Code'];
+    this.country = this.countryService.getCountryByAlphaCode(countryAlphaCode);
+  }
 }
